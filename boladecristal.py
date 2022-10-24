62a08528e7fa95db27ec34e72221aa431f12ca2c
Jovic <victosousa92@gmail.com>
2022-10-24 18:55:02 -0300

    bola de cristal adicionada

---
+++ boladecristal.py
@@ -0,0 +1,41 @@
+
+import time
+import random
+
+("🔮"*10)
+(print ("Faça qualquer pergunta de sim ou não, \n e eu sabiamente responderei"))
+("🔮"*10)
+
+time.sleep(2)
+print (" ")
+
+pergunta = (str(input("Sua pergunta: ")))
+
+resposta = random.randrange (1,5)
+if resposta == 1:
+    time.sleep(2)
+    print ("*✧･ﾟ: *✧･ﾟ:")
+    time.sleep(1)
+    print ("Sim")
+
+if resposta == 2:
+    time.sleep(2)
+    print ("*✧･ﾟ: *✧･ﾟ:")
+    time.sleep(1)
+    print ("Não")
+
+if resposta == 3:
+    time.sleep(2)
+    print ("*✧･ﾟ: *✧･ﾟ:")
+    time.sleep(1)
+    print ("Talvez")
+
+if resposta == 4:
+    time.sleep(2)
+    print ("*✧･ﾟ: *✧･ﾟ:")
+    time.sleep(1)
+    print ("Complicado")
+
+
+
+
